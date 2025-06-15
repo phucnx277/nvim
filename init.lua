ae77898479a4ff9f80db9ef7957a8a75ac331030
Phuc Nguyen <phucnx277@gmail.com>
2025-06-15 22:02:19 +0700

    conform x prettier x {t|j}sx

--- init.lua
+++ init.lua
@@ -787,6 +787,9 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettier', stop_after_first = true },
+        typescript = { 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettier', stop_after_first = true },
       },
     },
   },
